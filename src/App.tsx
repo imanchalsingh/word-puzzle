812b4b02056176a91b292d6706c85f55e387e25a
anchalDev159357 <singhanchal159357@gmail.com>
2024-12-24 21:28:09 +0530

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 2500); // 2.5 seconds wait
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!isLoaded && (
+        <div id="loader">
+          <div className="avatar"></div>
+        </div>
+      )}
+      {isLoaded && (
+        <iframe
+          id="wordgus-frame"
+          src="https://wordgus.vercel.app"
+          allowFullScreen
+          title="WordGus"
+        />
+      )}
     </div>
   );
 }
